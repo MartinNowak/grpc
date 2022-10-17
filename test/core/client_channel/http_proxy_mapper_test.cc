f48b5081f7cc83bdef9a58256b69c73f89a6ef6c
MartinNowak <MartinNowak@users.noreply.github.com>
2022-10-17 05:52:16 +0000

    Automated change: Fix sanity tests

--- test/core/client_channel/http_proxy_mapper_test.cc
+++ test/core/client_channel/http_proxy_mapper_test.cc
@@ -45,13 +45,15 @@ class ScopedSetEnv {
 // https://github.com/grpc/grpc/issues/26548
 TEST(ProxyTest, UserInfo) {
   std::map<std::string, std::string> test_cases = {
-    // echo -n user:pass | base64
-    {"user:pass", "dXNlcjpwYXNz"},
-    // echo -n user@google.com:pass | base64
-    {"user%40google.com:pass", "dXNlckBnb29nbGUuY29tOnBhc3M="},
+      // echo -n user:pass | base64
+      {"user:pass", "dXNlcjpwYXNz"},
+      // echo -n user@google.com:pass | base64
+      {"user%40google.com:pass", "dXNlckBnb29nbGUuY29tOnBhc3M="},
   };
   for (const auto& test_case : test_cases) {
-    auto args = ChannelArgs().Set(GRPC_ARG_HTTP_PROXY, absl::StrCat("http://", test_case.first, "@proxy.google.com"));
+    auto args = ChannelArgs().Set(
+        GRPC_ARG_HTTP_PROXY,
+        absl::StrCat("http://", test_case.first, "@proxy.google.com"));
     EXPECT_EQ(HttpProxyMapper().MapName("dns:///test.google.com:443", &args),
               "proxy.google.com");
     EXPECT_EQ(args.GetString(GRPC_ARG_HTTP_CONNECT_SERVER),
